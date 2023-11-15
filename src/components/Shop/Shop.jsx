5ea58282e2365c13ca73479b1dff79d66eb98430
naimurcse <2015naim@gmail.com>
2023-11-15 17:50:01 +0600

    Created a function to make a newCart of local storage data

--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import "./Shop.css";
 import Cart from '../Cart/Cart';
 import Product from '../Product/Product';
+import { addToDatabase, getShoppingCart } from '../../utilities/fake';
 
 const Shop = () => {
 
@@ -15,10 +16,28 @@ const Shop = () => {
     }, [])
     // console.log(products);
 
+    useEffect(() => {
+        const storedCart = getShoppingCart();
+        
+        const newCart = [];
+        for (let productId in storedCart) {
+            console.log(productId);
+            const exist = products.find(pd => pd.id === productId);
+            console.log(exist);
+            if (exist) {
+                exist.quantity = storedCart[productId];
+            }
+            newCart.push(exist);
+        }
+        // setCart(newCart);
+        console.log(newCart);
+    },[products])
     
     const handleAddToCart = (product) => {
     const newCart = [...cart,product]
         setCart(newCart);
+        addToDatabase(product.id);
+
     }
     // console.log(cart);
 
